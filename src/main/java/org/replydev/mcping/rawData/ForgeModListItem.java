e9b3a2dcaa4ff0525bc7fec04430f1ed01aa86d0
replydev <commoncargo@tutanota.com>
2022-11-21 23:49:32 +0100

    Initial source ported from Quboscanner

---
+++ src/main/java/org/replydev/mcping/rawData/ForgeModListItem.java
@@ -0,0 +1,20 @@
+package me.replydev.mcping.rawData;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ForgeModListItem {
+
+    @SerializedName("modid")
+    private String modid;
+
+    @SerializedName("version")
+    private String version;
+
+    public String getModid() {
+        return modid;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+}
